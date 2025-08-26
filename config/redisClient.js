7f3b6fadf08aa1e908135b6994a1540dafe780cc
ShreyashWebCoder <shreyashwebcoder@gmail.com>
2025-08-26 16:31:35 +0530

    edit redis

--- config/redisClient.js
+++ config/redisClient.js
@@ -1,17 +1,31 @@
 const redis = require("redis");
 
+// const client = redis.createClient({
+//     username: process.env.REDIS_USERNAME,
+//     password: process.env.REDIS_PASSWORD,
+//     socket: {
+//         host: process.env.REDIS_URL,
+//         port: process.env.REDIS_PORT
+//     }
+//     // url: process.env.REDIS_URL,
+// });
+
 const client = redis.createClient({
-    url: process.env.REDIS_URL,
+    url: process.env.REDIS_URL  
 });
 
 client.on("error", (err) => console.error("❌ Redis Error", err));
+client.on("connect", () => console.log("✅ Redis Connected Successfully"));
 
 (async () => {
     try {
 
         if (!client.isOpen) {
             await client.connect();
-            console.log("✅ Redis Connected Successfully...");
+            await client.set("test", "test");
+            const result = await client.get("test");
+            if (result === "test") console.log("✅ Redis Test Passed");
+
         }
         return client;
     } catch (error) {
